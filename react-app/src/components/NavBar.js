6cfd3320934a33c9ad435e2aa53f4f6d81dd7068
Daniel Upchurch <danielupchurch93@gmail.com>
2021-03-02 13:44:24 -0500

    tailwind set up and working

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,25 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
 
 const NavBar = ({ setAuthenticated }) => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to="/" exact={true} activeClassName="active">
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/login" exact={true} activeClassName="active">
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/sign-up" exact={true} activeClassName="active">
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users" exact={true} activeClassName="active">
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton setAuthenticated={setAuthenticated} />
-        </li>
-      </ul>
+    <nav className="md:flex mx:auto flex-row p-2 m-2">
+      <NavLink className="flex-row p-2 m-2" to="/" exact={true} activeClassName="active">
+        Home
+      </NavLink>
+      <NavLink className="flex-row p-2 m-2" to="/login" exact={true} activeClassName="active">
+        Login
+      </NavLink>
+      <NavLink className="flex-row p-2 m-2"to="/sign-up" exact={true} activeClassName="active">
+        Sign Up
+      </NavLink>
+      <NavLink className="flex-row p-2 m-2" to="/users" exact={true} activeClassName="active">
+        Users
+      </NavLink>
+      <LogoutButton className="flex-row p-2 m-2" setAuthenticated={setAuthenticated} />
     </nav>
   );
-}
+};
 
 export default NavBar;
