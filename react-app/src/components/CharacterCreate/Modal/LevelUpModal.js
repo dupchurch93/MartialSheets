1ac608082c3d6cb963327592b23949fc225e9c8e
Daniel Upchurch <danielupchurch93@gmail.com>
2021-03-17 17:22:00 -0400

    feature choices working

--- react-app/src/components/CharacterCreate/Modal/LevelUpModal.js
+++ react-app/src/components/CharacterCreate/Modal/LevelUpModal.js
@@ -1,5 +1,6 @@
 import { TiDelete } from "react-icons/ti";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import FeatureList from "../FeaturesColumn/FeatureList";
 
 const LevelUpModal = ({
@@ -7,15 +8,17 @@ const LevelUpModal = ({
   characterClass,
   setModal,
   handleSubmit,
-  setFeatures,
   features,
 }) => {
   const hidden = modal ? "modal" : "hidden";
+  const [pickedFeature, setPickedFeature] = useState("Select Feature Option");
+  const [featureHelp, setFeatureHelp] = useState("Choice Description")
 
   const closeModal = (e) => {
     e.preventDefault();
     setModal(false);
   };
+  // console.log('controlled state feature', pickedFeature)
 
   const classFeatures = useSelector((state) => state.features[characterClass]);
 
@@ -28,13 +31,18 @@ const LevelUpModal = ({
     if (featureChoices.length > 0) {
       choiceName = featureChoices[0].source.split(":")[4];
     }
+  };
+
+  const handlePickedFeature = (featureName) => {
+    setPickedFeature(featureName);
+    setFeatureHelp(featureChoices.filter((feature) => feature.name === featureName)[0].description)
   }
 
   return (
     <div
-      className={`fixed m-0 ${hidden} w-full h-full bg-gray-900 bg-opacity-20 top-0 flex justify-center`}
+      className={`fixed m-0 ${hidden} w-full h-full bg-gray-900 bg-opacity-30 top-0 flex justify-center`}
     >
-      <div className="bg-white border-2 border-black rounded-lg min-w-characterSheet my-16 flex items-center flex-col">
+      <div className="modalInfo bg-white border-2 border-black rounded-lg min-w-characterSheet my-16 flex items-center flex-col overflow-y-auto">
         <div className="w-full flex justify-end p-2">
           <button onClick={(e) => closeModal(e)} className="rounded-lg">
             <TiDelete />
@@ -43,23 +51,48 @@ const LevelUpModal = ({
         <div className="font-bold underline text-lg m-4">
           Your {characterClass} begins their journey.
         </div>
-        <div>Feautres gained on this level:</div>
+        <div>Features gained on this level:</div>
         <FeatureList features={features}></FeatureList>
-        {featureChoices ? (
-          <>
-            <div>Please choose your following class feature: {choiceName}</div>
-            {featureChoices.map((feature) => {
-              return <div key={feature.name}>{feature.name}</div>;
-            })}
-            <div>
-              Finally a submit button that actually creates the character or
-              submits the changes
+        {featureChoices && featureChoices.length > 0 ? (
+          <div className="">
+            {console.log("render")}
+            <div className="border border-black p-2 m-2 rounded-lg w-80">
+              <div className="m-4">
+                Please choose your following class feature: {choiceName}.
+              </div>
+              <select
+                value={pickedFeature}
+                name="pickedFeature"
+                onChange={(e) => handlePickedFeature(e.target.value)}
+                className="w-full border border-black rounded-lg"
+              >
+                <option disabled value="Select Feature Option" hidden>
+                  Select Feature Option
+                </option>
+                {featureChoices.map((feature) => {
+                  return (
+                    <option key={feature.name} value={feature.name}>
+                      {feature.name}
+                    </option>
+                  );
+                })}
+              </select>
             </div>
-          </>
+            <div className="border border-black p-2 m-2 rounded-lg w-80">
+              <div className="font-bold underline m-4 text-center">
+                Choice Description
+              </div>
+              <div>{featureHelp}</div>
+            </div>
+          </div>
         ) : (
-          <div></div>
+          <div>test</div>
         )}
-        <button onClick={handleSubmit} type="submit">
+        <button
+          onClick={handleSubmit}
+          className="w-56 p-2 rounded-lg m-4 border border-black bg-myred text-white"
+          type="submit"
+        >
           Finalize Character
         </button>
       </div>
